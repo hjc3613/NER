25869534c217419a795e3f2d0d9f361b2f9fa975
hujunchao <1249952064@qq.com>
2023-01-02 09:32:09 +0800

    修复metric bug

--- metric/metric_utils.py
+++ metric/metric_utils.py
@@ -1,5 +1,11 @@
 import sys
 sys.path.append('.')
+
+from typing import Tuple
+
+from sklearn.metrics import classification_report as sklearn_class_report
+import numpy as np
+
 from config.config_utils import *
 
 def get_ent_(start_pos, end_pos, mask, input_ids, longer_best):
@@ -7,7 +13,7 @@ def get_ent_(start_pos, end_pos, mask, input_ids, longer_best):
     shape: [bsz, label_nums, seq_len]
     '''
     result = []
-    for sent_idx, start_tensor, end_tensor, mask_, sentence_ids in enumerate(zip(start_pos, end_pos, mask, input_ids)):
+    for sent_idx, (start_tensor, end_tensor, mask_, sentence_ids) in enumerate(zip(start_pos, end_pos, mask, input_ids)):
         for label_idx in range(len(LABELS)):
             type_ = IDX2LABEL[label_idx]
             start_idx_lst = [idx for idx, val in enumerate(start_tensor[label_idx][1:sum(mask_)]) if val==1]
@@ -22,12 +28,44 @@ def get_ent_(start_pos, end_pos, mask, input_ids, longer_best):
                         entity = ''.join(TOKENIZER.convert_ids_to_tokens(sentence_ids[start:end+1]))
                         result.append((sent_idx, start, end, type_, entity))
                         cur_end = end
+                        break
     return result
 
 def cal_precision_recall_f(origin, pred, thresh):
+    print(f'######################### {thresh} ##############################')
+    print(f'ground true\n: {origin[:30]} \n...\n {origin[-30:]}')
+    print(f'predict result\n: {pred[:30]} \n...\n {pred[-30:]}')
     intersection = set(origin).intersection(set(pred))
-    precision = len(intersection) / len(set(pred))
-    recall = len(intersection) / len(set(origin))
-    f = 2*precision*recall/(precision+recall)
-    result = {f'precision_{thresh}':precision, f'recall_{thresh}':recall, f'f_{thresh}':f}
+    if len(intersection) == 0:
+        precision = 0
+        recall = 0
+        f = 0
+    else:
+        precision = len(intersection) / len(set(pred))
+        recall = len(intersection) / len(set(origin))
+        f = 2*precision*recall/(precision+recall)
+        result = {f'precision_{thresh}':precision, f'recall_{thresh}':recall, f'f_{thresh}':f}
     return result
+
+def classification_report(pred:np.ndarray, origin:np.ndarray, mask:np.ndarray):
+    '''
+    shape: [bsz, label_num, seq_len]
+    '''
+    pred_ = {}
+    origin_ = {}
+    # 遍历每个example
+    for pred_pos, origin_pos, mask_ in zip(pred, origin, mask):
+        seq_len = sum(mask_)
+        # 遍历每个label
+        for label_idx, (pred_seq, origin_seq) in enumerate(zip(pred_pos, origin_pos)):
+            label_name = IDX2LABEL[label_idx]
+            pred_seq = list(pred_seq)
+            origin_seq = list(origin_seq)
+            pred_[label_name] = pred_.get(label_name, []) + pred_seq
+            origin_[label_name] = origin_.get(label_name, []) + origin_seq
+
+    for label_name in LABELS:
+        pred_label = pred_[label_name]
+        origin_label = origin_[label_name]
+        print(f'############################## {label_name} ##############################')
+        sklearn_class_report(y_pred=pred_label, y_true=origin_label)
--- run_ner.py
+++ run_ner.py
@@ -1,11 +1,12 @@
 from functools import partial
-
+import os
+os.environ["WANDB_DISABLED"] = "true"
 from dataset.data_utils import CMeEEDataset, collate_fn_no_lavel_vec
 from config.config_utils import *
 from torch.utils.data import DataLoader
 from transformers import Trainer, TrainingArguments, AdamW, get_linear_schedule_with_warmup, EvalPrediction
 from models.BertSpanForNER import BertSpanForNER
-from metric.metric_utils import get_ent_, cal_precision_recall_f
+from metric.metric_utils import get_ent_, cal_precision_recall_f, classification_report
 
 
 get_ent = partial(get_ent_, longer_best=False)
@@ -18,10 +19,14 @@ def compute_metrics(eval_pred: EvalPrediction):
     origin_labels = get_ent(*label_ids, mask, input_ids)
     predictions = eval_pred.predictions
     for thresh in [0.4, 0.5, 0.6, 0.7, 0.8]:
-        predictions_ = [i > thresh for i in predictions]
+        predictions_ = [(i > thresh).astype('int') for i in predictions]
         pred_labels = get_ent(*predictions_, mask, input_ids)
-        metric_ = cal_precision_recall_f(origin=origin_labels, pred=pred_labels)
+        metric_ = cal_precision_recall_f(origin=origin_labels, pred=pred_labels, thresh=thresh)
         result.update(metric_)
+        print('起始位置指标：\n')
+        classification_report(predictions_[0], label_ids[0], mask)
+        print('结束未知指标: \n')
+        classification_report(predictions_[1], label_ids[1], mask)
     return result
 
 def main():
@@ -30,20 +35,24 @@ def main():
         overwrite_output_dir=True,
         do_eval=True,
         do_train=True,
-        evaluation_strategy='epoch',
+        # evaluation_strategy='epoch',
+        evaluation_strategy='steps',
+        eval_steps=300,
         per_device_train_batch_size=10,
         num_train_epochs=5,
         # learning_rate=...
         weight_decay=0.001,
         warmup_ratio=0.15,
         lr_scheduler_type='linear',
-        no_cuda=True,
+        # no_cuda=True,
         include_inputs_for_metrics=True,
         metric_for_best_model='f_0.5',
-        greater_is_better=True
+        greater_is_better=True,
+        save_steps=300,
+        gradient_accumulation_steps=6
     )
-    ds_train = CMeEEDataset('CMeEE/CMeEE_train.json')[:20]
-    ds_dev = CMeEEDataset('CMeEE/CMeEE_dev.json')[:15]
+    ds_train = CMeEEDataset('CMeEE/CMeEE_train.json')
+    ds_dev = CMeEEDataset('CMeEE/CMeEE_dev.json')
     if args.max_steps > 0:
         t_total = args.max_steps
         args.num_train_epochs = args.max_steps // (len(ds_train) // args.gradient_accumulation_steps) + 1
